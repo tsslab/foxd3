9c9f73ba3fa94180586d75efe4bd8d4d34a096e6
tsslab <tatjana.sauka-spengler@imm.ox.ac.uk>
2017-11-06 17:09:00 +0000

    Update R_code.R

--- PAGODA_analysis/R/R_code.R
+++ PAGODA_analysis/R/R_code.R
@@ -8,4 +8,4 @@ library(scde)
 app = readRDS("pagoda_app_86_cells")
 
 # show app in the browser (port 1468)
-show.app(app, “foxd3”, browse = TRUE, port = 1468)
+show.app(app, "foxd3", browse = TRUE, port = 1468)
