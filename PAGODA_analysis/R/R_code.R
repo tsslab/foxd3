f41ef244c467f130fd566d3635010f7b17389a3b
tsslab <tatjana.sauka-spengler@imm.ox.ac.uk>
2017-11-06 14:13:03 +0000

    Update R_code.R

--- PAGODA_analysis/R/R_code.R
+++ PAGODA_analysis/R/R_code.R
@@ -5,7 +5,7 @@ devtools::install_github('hms-dbmi/scde', build_vignettes = FALSE)
 }
 library(scde)
 
-app = readRDS("/data/pagoda_app_86_cells")
+app = readRDS("pagoda_app_86_cells")
 
 # show app in the browser (port 1468)
 show.app(app, “foxd3”, browse = TRUE, port = 1468)
